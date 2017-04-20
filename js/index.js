a1bab5bbb5ae51995e6d7ac3727ad2040222b20c
Tiffany Lam <tlam5@sbcglobal.net>
2017-04-19 23:03:34 -0700

    Include show page, add, and delete routes

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@ const methodOverride = require('method-override');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
@@ -22,12 +23,23 @@ var id = 1;
 
 // '/'
 app.get('/', function(req, res, next) {
-  res.send('Hellooooo');
+  res.redirect('/collection');
 });
 
 // '/collection'
+app.get('/collection', function(req, res, next) {
+  res.render('show', {collection});
+});
+
 // '/add'
+app.post('/add', function(req, res, next) {
+  collection.push();
+});
+
 // '/delete'
+app.delete('/collection/:id', function(req, res, next) {
+  
+});
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000');
