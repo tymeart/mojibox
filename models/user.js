aa287979778e7bb6d07d3202b067615196e45cd0
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-10 18:23:04 -0700

    Make emoticons an array of objects in user schema

--- models/user.js
+++ models/user.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: String,
-  emoticons: {
+  emoticons: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Emoticon'
-  }
+  }]
 });
 
 const User = mongoose.model('User', userSchema);
