308d62e6245f187fcf2f8c31c54708a95b6bb670
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-12 00:18:03 -0700

    Lay out userRoutes

--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,40 @@ const express = require('express'),
       router = express.Router(),
       db = require('../models');
 
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.get('/:id', function(req, res, next) {
+  db.User.findById(req.params.id).then(function(user) {
+    res.render('users/show', {user}),
+    function(err) {
+      res.send('Error!');
+    });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('users/new');
+});
+
+router.get('/:id/edit', function(req, res, next) {
+  db.User.findById(req.params.id).then(function(user) {
+    res.render('users/edit', {user}),
+    function(err) {
+      res.send('Error!');
+    });
+});
+
+router.post('/', function(req, res, next) {
+
+});
+
+router.patch('/:id', function(req, res, next) {
+
+});
+
+router.delete('/:id', function(req, res, next) {
+
+});
+
 module.exports = router;
