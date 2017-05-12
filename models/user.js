db675d06f5c23fa93d237fbdeda32657f8ade464
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-12 16:00:39 -0700

    Add password property to user model

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: String,
+  password: String,
   emoticons: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Emoticon'
