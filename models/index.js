9a636cf816f08bb36cdb5c7af69a5f208546adc7
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-12 00:16:59 -0700

    Remove redundant mongoose.connect()

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
-mongoose.connect('mongodb://localhost/mojibox');
 mongoose.Promise = Promise;
 
 module.exports.User = require('./user');
