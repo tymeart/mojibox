26fc5245535c051401af4c7ce77d4dd8ce0c8dd2
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-16 15:14:44 -0700

    Chain .catch() onto .then() for error handling

--- routes/users.js
+++ routes/users.js
@@ -13,18 +13,24 @@ router.get('/new', function(req, res, next) {
 router.get('/:username', function(req, res, next) {
   db.User.findOne({username: req.params.username}).then(function(user) {
     res.render('users/show', {user});
+  }).catch(function(err) {
+    console.log(err);
   });
 });
 
 router.get('/:username/edit', function(req, res, next) {
   db.User.findOne({username: req.params.username}).then(function(user) {
     res.render('users/edit', {user});
+  }).catch(function(err) {
+    console.log(err);
   });
 });
 
 router.post('/', function(req, res, next) {
   db.User.create(req.body).then(function() {
     res.redirect('/');
+  }).catch(function(err) {
+    console.log(err);
   });
 });
 
@@ -32,6 +38,8 @@ router.post('/', function(req, res, next) {
 router.patch('/:username', function(req, res, next) {
   db.User.findOneAndUpdate({username: req.params.username}, {username: req.body.newUsername}).then(function(user) {
     res.redirect('/');
+  }).catch(function(err) {
+    console.log(err);
   });
 });
 
@@ -39,6 +47,8 @@ router.patch('/:username', function(req, res, next) {
 router.delete('/:username', function(req, res, next) {
   db.User.findOneAndRemove({username: req.params.username}).then(function() {
     res.redirect('/');
+  }).catch(function(err) {
+    console.log(err);
   });
 });
 
