4e0dc83f7c0b2b4e586ebb8416634386c60c1157
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-23 12:49:47 -0700

    Save modified user emoticons array

--- routes/emoticons.js
+++ routes/emoticons.js
@@ -4,9 +4,7 @@ const express = require('express'),
 
 router.get('/', function(req, res, next) {
   db.User.findOne({username: req.params.username}).populate('emoticons').exec(function(err, user) {
-    if (err) {
-      console.log(err);
-    }
+    if (err) console.log(err);
     res.render('emoticons/index', {emoticons: user.emoticons});
   });
 });
@@ -19,6 +17,11 @@ router.post('/new', function(req, res, next) {
   db.User.findOne({username: req.params.username}).then(function(user) {
     db.Emoticon.create({content: req.body.content, category: req.body.category, user: user._id}).then(function(newEmoticon) {
       user.emoticons.push(newEmoticon);
+      user.markModified('emoticons');
+      user.save(function(err) {
+        if (err) console.log(err);
+        console.log('Saved!');
+      });
       res.redirect(`/user/${user.username}/collection`);
     }).catch(function(err) {
       console.log(err);
