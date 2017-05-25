2e58bef09b556253492eebdd397d0aaca6db76a5
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-25 14:35:49 -0700

    Toggle selected class

--- public/js/select.js
+++ public/js/select.js
@@ -3,7 +3,7 @@ var emoticonsContainer = document.getElementById('emoticons');
 emoticonsContainer.addEventListener('click', function(event) {
   if (event.target.matches('.emoticon')) {
     event.stopPropagation();
-    event.target.classList.add('selected');
+    event.target.classList.toggle('selected');
     selected.push(event.target);
   }
 });
