4dfefe041dfb1305d0b8e424ccfdbd3912f0c3a3
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-25 14:25:01 -0700

    Rename click handler file

--- public/js/select.js
+++ public/js/select.js
@@ -3,6 +3,7 @@ var emoticonsContainer = document.getElementById('emoticons');
 emoticonsContainer.addEventListener('click', function(event) {
   if (event.target.matches('.emoticon')) {
     event.stopPropagation();
+    event.target.classList.add('selected');
     selected.push(event.target);
   }
 });
