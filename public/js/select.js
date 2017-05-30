3bde929a83ee04655b485dc57712776407252d2d
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-30 15:09:12 -0700

    Create and call toggleSelectedArrItem function

--- public/js/select.js
+++ public/js/select.js
@@ -4,6 +4,15 @@ var selectModeToggle = document.getElementById('selectModeToggle');
 var deleteButton = document.getElementById('delete');
 var emoticonsContainer = document.getElementById('emoticons');
 
+function toggleSelectedArrItems(emoticon) {
+  var index = selected.indexOf(emoticon);
+  if (index === -1) {
+    selected.push(emoticon);
+  } else {
+    selected.splice(index, 1);
+  }
+}
+
 selectModeToggle.addEventListener('click', function() {
   if (selectMode) {
     selectMode = false;
@@ -11,11 +20,13 @@ selectModeToggle.addEventListener('click', function() {
     deleteButton.classList.add('hide');
   } else {
     selectMode = true;
+    // change style of select link to indicate select mode is ON somehow
     deleteButton.classList.remove('hide');
     emoticonsContainer.addEventListener('click', function(event) {
       if (event.target.matches('.emoticon')) {
         event.stopPropagation();
         event.target.classList.toggle('selected');
+        toggleSelectedArrItems(event.target.innerHTML);
       }
     });
   }
