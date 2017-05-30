84c7bc0de708a785cbce296e147087fa6d745152
Tiffany Lam <tlam5@sbcglobal.net>
2017-05-30 13:20:49 -0700

    Add a select mode and ability to toggle it

--- public/js/select.js
+++ public/js/select.js
@@ -1,9 +1,22 @@
+var selectMode = false;
 var selected = [];
+var selectModeToggle = document.getElementById('selectModeToggle');
+var deleteButton = document.getElementById('delete');
 var emoticonsContainer = document.getElementById('emoticons');
-emoticonsContainer.addEventListener('click', function(event) {
-  if (event.target.matches('.emoticon')) {
-    event.stopPropagation();
-    event.target.classList.toggle('selected');
-    selected.push(event.target);
+
+selectModeToggle.addEventListener('click', function() {
+  if (selectMode) {
+    selectMode = false;
+    // change style of select link to indicate select mode is OFF somehow
+    deleteButton.classList.add('hide');
+  } else {
+    selectMode = true;
+    deleteButton.classList.remove('hide');
+    emoticonsContainer.addEventListener('click', function(event) {
+      if (event.target.matches('.emoticon')) {
+        event.stopPropagation();
+        event.target.classList.toggle('selected');
+      }
+    });
   }
 });
