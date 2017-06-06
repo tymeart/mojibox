6da9da4427904de93b721d94e023e71162e08b13
Tiffany Lam <tlam5@sbcglobal.net>
2017-06-06 14:36:34 -0700

    Require user and emoticon models and remove db from instances in the routes

--- routes/emoticons.js
+++ routes/emoticons.js
@@ -1,9 +1,10 @@
 const express = require('express'),
       router = express.Router({mergeParams: true}),
-      db = require('../models');
+      User = require('../models/user');
+      Emoticon = require('../models/emoticon');
 
 router.get('/', function(req, res, next) {
-  db.User.findOne({username: req.params.username}).populate('emoticons').exec(function(err, user) {
+  User.findOne({username: req.params.username}).populate('emoticons').exec(function(err, user) {
     if (err) console.log(err);
     res.render('emoticons/index', {user, emoticons: user.emoticons});
   });
@@ -14,8 +15,8 @@ router.get('/new', function(req, res, next) {
 });
 
 router.post('/new', function(req, res, next) {
-  db.User.findOne({username: req.params.username}).then(function(user) {
-    db.Emoticon.create({content: req.body.content, category: req.body.category, user: user._id}).then(function(newEmoticon) {
+  User.findOne({username: req.params.username}).then(function(user) {
+    Emoticon.create({content: req.body.content, category: req.body.category, user: user._id}).then(function(newEmoticon) {
       user.emoticons.push(newEmoticon);
       user.markModified('emoticons');
       user.save(function(err) {
