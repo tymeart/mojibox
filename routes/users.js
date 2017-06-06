bf2160400048e527fa8b90ba8cc1324eed7dbe39
Tiffany Lam <tlam5@sbcglobal.net>
2017-06-06 13:42:32 -0700

    Add / before user/login path so app redirects properly

--- routes/users.js
+++ routes/users.js
@@ -81,7 +81,7 @@ function isLoggedIn(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('user/login');
+  res.redirect('/user/login');
 }
 
 module.exports = router;
