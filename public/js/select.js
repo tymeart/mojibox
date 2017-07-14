4a22269fcab3b644d92d97f532dbf197604470d3
Tiffany Lam <tlam5@sbcglobal.net>
2017-07-14 00:14:58 -0700

    Fix #1

--- public/js/select.js
+++ public/js/select.js
@@ -18,40 +18,19 @@ selectModeToggle.addEventListener('click', function() {
   if (selectMode === true) {
     selectMode = false;
     deleteButton.classList.add('hide');
-    // select mode OFF
   } else if (selectMode === false){
     selectMode = true;
     deleteButton.classList.remove('hide');
-    // select mode ON
-    emoticonsContainer.addEventListener('click', function(event) {
-      if (event.target.matches('.emoticon')) {
-        event.stopPropagation();
-        event.target.classList.toggle('selected');
-        toggleSelectedArrItems(event.target.id);
-      }
-    });
   }
 });
 
-// toggleSelectedArrItems outside of click handler??
-// iterate through the emoticons with selected class
-// document.getElementsByClassName('selected')
-// pass ids into toggleSelectedArrItems
-
-// selectModeToggle.addEventListener('click', function() {
-//   if (selectMode) {
-//     selectMode = false;
-//     deleteButton.classList.add('hide');
-//   } else {
-//     selectMode = true;
-//     deleteButton.classList.remove('hide');
-//     // toggle emoticons in/out of selected array
-//     emoticonsContainer.addEventListener('click', function(event) {
-//       if (event.target.matches('.emoticon')) {
-//         event.stopPropagation();
-//         event.target.classList.toggle('selected');
-//         toggleSelectedArrItems(event.target.id);
-//       }
-//     });
-//   }
-// });
+emoticonsContainer.addEventListener('click', function(event) {
+  if (!selectMode) {
+    return;
+  }
+  if (event.target.matches('.emoticon')) {
+    event.stopPropagation();
+    event.target.classList.toggle('selected');
+    toggleSelectedArrItems(event.target.id);
+  }
+});
